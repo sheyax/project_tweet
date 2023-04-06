aad2cc3a681f24c5b3be555455bc73d3112fa856
sheyax <olusheyax@gmail.com>
2023-04-06 14:37:45 +0000

    Create 26 files

---
+++ proj-tweet/components/Input.tsx
@@ -0,0 +1,19 @@
+interface InputProps {
+  placeholder?: string;
+  value?: string;
+  type?: string;
+  disabled?: boolean;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  value,
+  type,
+  disabled,
+  onChange,
+}) => {
+  return <></>;
+};
+
+export default Input;
